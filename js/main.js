2c8e38364d1978d4a1e22ab214809356baa6c019
avazbek9999 <avazbe97salimov@yandex.ru>
2021-07-10 11:39:00 +0500

    add terminator

--- js/main.js
+++ js/main.js
@@ -18,21 +18,21 @@
 
 var elparagraph = document.querySelector('.paragraph')
 
-var where = prompt(`Qayerga sayohat qilmoqchisiz?`);
+var where = prompt(`Qayerga sayohat qilmoqchisiz?`)
 alert(
 	`${where}ga ketadigan  barcha harajatlar: \n -Borish-kelish samalyot bileti-$500 \n -Mehmonxona to'lovi(to'liq safar davomiyligi uchun)-120$ \n -Muzey va ko'ngil ochar joylar uchun-120 yevro`
-);
-alert(`Hozirgi dollor va yevro kurslari: \n1$=9433.42 \n1 yevro=10254.03`);
+)
+alert(`Hozirgi dollor va yevro kurslari: \n1$=9433.42 \n1 yevro=10254.03`)
 
-var tisket = Math.round(500 * 9433.42);
-var hotel = Math.round(120 * 9433.42);
-var muzey = Math.round(120 * 10254.03);
-var expense = Math.round(tisket + hotel + muzey);
+var tisket = Math.round(500 * 9433.42)
+var hotel = Math.round(120 * 9433.42)
+var muzey = Math.round(120 * 10254.03)
+var expense = Math.round(tisket + hotel + muzey)
 
-var firstName = prompt('Ismingizni kiriting');
+var firstName = prompt('Ismingizni kiriting')
 var money = prompt(
 	`${firstName} o'zingizda qancha pul borligini so'mda kiriting`
-);
+)
 
 if (money >= expense) {
 	elparagraph.textContent = `Oq yo'l ${firstName}`
